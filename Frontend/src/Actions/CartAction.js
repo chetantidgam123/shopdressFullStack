1d1388ed26156a7b2feacf9cd9540b949ec27e53
chetan <ctidgam1997@gmail.com>
2022-12-27 13:14:48 +0530

    first

---
+++ Frontend/src/Actions/CartAction.js
@@ -0,0 +1,46 @@
+import { json } from "react-router-dom";
+
+export  const GetCartData = (dispatch)=>{
+
+   
+       async function GetData(){
+        // let res = await fetch('https://dead-gold-binturong-kilt.cyclic.app/cart');
+        let data= JSON.parse(localStorage.getItem('CartData'))||[];
+    
+        dispatch({
+            type:"GETCARTDATA",
+            payload:data
+        })
+
+       }
+       GetData()
+}
+export  const GetPatchData = async (Data,id,dispatch)=>{
+    await fetch(`https://dead-gold-binturong-kilt.cyclic.app/cart/${id}`,{
+      method:"PATCH",
+      body: JSON.stringify(Data),
+      headers :{
+      "Content-Type":"application/json",
+      },   
+     });
+ 
+
+}
+export const GetDeleteData = async (id,index,Array,dispatch)=>{
+      
+      let temp=[]
+      Array.splice(index,1);
+ 
+         temp=[...Array]
+     
+         dispatch({
+             type:"GETCARTDATA",
+             payload:temp
+      })
+    //   await fetch(`https://dead-gold-binturong-kilt.cyclic.app/cart/${id}`,{
+    //      method:'DELETE'
+    //   });
+    localStorage.setItem('CartData',JSON.stringify(temp));
+      
+      
+}     
---
+++ Frontend/src/Component/Cart/Cartt.js
@@ -0,0 +1,131 @@
+import store from "../../Store/Store"
+import { GetCartData,GetPatchData,GetDeleteData } from '../../Actions/CartAction';
+export default function CARTMENU (){
+    if(document.getElementById("CARTDI")){
+        document.getElementById("CARTDI").id="CARTDIV"
+        localStorage.setItem("Cartisopen","true")
+    }else{
+        document.getElementById("CARTDIV").id="CARTDI";
+        localStorage.setItem("Cartisopen","false")
+    }
+    
+}
+
+export const SearchDiv=(dispatch)=>{
+
+   
+    if(document.getElementById("SearchDi")){
+        document.getElementById("SearchDi").id="SearchDiv"
+    
+         localStorage.setItem("Searchisopen","true")
+    }else{
+        dispatch({
+            type:"GETCARTDATA",
+            payload:[]
+        })
+        document.getElementById("SearchDiv").id="SearchDi"
+        localStorage.setItem("Searchisopen","false")
+    }
+}
+
+
+export const DecreaseQTY=(id,dispatch)=>{
+    let temp=[]
+    let Array=store.getState().ShopDressReducer.Cart;
+    console.log("ok-des");
+    Array.map((el, index,ar)=>{
+        if(el.id==id){
+           if(Number(el.Qty)>1){
+             el.Qty= Number(el.Qty)-1; 
+            //   GetPatchData(el, el.id);
+           }else{
+             GetDeleteData(el.id,index,Array,dispatch);
+           }
+        }
+    })
+    temp=[...Array]
+    dispatch({
+        type:"GETCARTDATA",
+        payload:temp
+    })
+    localStorage.setItem('CartData',JSON.stringify(temp));
+    
+}
+
+
+
+export const IncreaseQTY=(id,dispatch, Array)=>{
+    let temp=[]
+     Array.map((el)=>{
+        if(el.id==id){
+            
+            el.Qty= Number(el.Qty)+1;
+            
+            
+            // GetPatchData(el,id);
+        }
+     })
+
+        temp=[...Array]
+        dispatch({
+            type:"GETCARTDATA",
+            payload:temp
+        })
+
+        localStorage.setItem('CartData',JSON.stringify(temp));
+}
+
+
+export const SearchBar=(dispatch)=>{
+    let ans=debounced(FetchSearhData,500,dispatch);
+    ans();
+    console.log("e.target.value")
+}
+function debounced(fn, delay, dispatch) {
+    let Timeout;
+    return function() {
+        console.log("fgf");
+      if (Timeout) clearTimeout(Timeout);
+
+      Timeout = setTimeout(function () {
+        fn(dispatch);
+      }, delay);
+    };
+    // df();
+} 
+async function FetchSearhData(dispatch){
+    let qure=document.getElementById('inputSearch').value;
+    let res = await fetch(`https://dead-gold-binturong-kilt.cyclic.app/Product_Data?q=${qure}`);
+    let ans= await res.json();
+    // console.log(ans)
+    dispatch({
+        type:"GETSEACHDATA",
+        payload:ans
+    })
+ 
+}
+
+export const ApplyCoupon = (e)=>{
+    if (e.key === 'Enter') {
+        let n=document.getElementById('CouponInput').value;
+        // let n=;
+        if(n=='Dressup10'){
+            let amount =Number( localStorage.getItem('Total_Price'));
+            let amound= (amount/100) * 10
+            // let amound=
+            localStorage.setItem('Total_Price',(amount-amound))
+            console.log("ok", (amount-amound) )
+            document.getElementById('GDP').innerHTML=`$ ${amount-amound}`
+            document.getElementById('ApplyCopounText').innerHTML=`Coupon Applied  `
+            document.getElementById('ApplyCopounText').style.background=`#c6f6d5`;
+            document.getElementById('ApplyCopounText').style.color=`#133f2b`;
+            document.getElementById('ApplyCopounText').style.background=`Coupon Applied  `
+        }   
+        console.log(n);
+    }
+
+    // if
+    // let o=e.target.value;
+    // if()
+     
+}
